24b26edfdd37975c51dda6a5767c29e4b53f56b0
Enny <enny.frans@condorcet.be>
2021-04-19 23:42:31 +0200

    structur basic

---
+++ admin/lib/php/autoload.php
@@ -0,0 +1,17 @@
+<?php
+//page de chargement automatique des classes
+
+//fontion qui charge les fichiers classes
+
+function autoload($nom_classe)
+{
+    if (file_exists('./lib/php/classes/' . $nom_classe . '.class.php')) {
+        require './lib/php/classes/' . $nom_classe . '.class.php';
+    } else if (file_exists('./admin/lib/php/classes/' . $nom_classe . '.class.php')) {
+        require './admin/lib/php/classes/' . $nom_classe . '.class.php';
+    }
+}
+
+spl_autoload_register('autoload');
+
+//fonction prèdifinie qui appelle la fonciton d'autoload lors d'une rencontre du mot new
